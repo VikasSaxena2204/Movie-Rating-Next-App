8b5d1c9b00bc342f47416a97c8304a81983f80b7
VIKAS SAXENA <vikassaxena123578@gmail.com>
2024-10-14 20:14:33 +0530

    Initial commit

--- src/app/search/[searchTerm]/page.jsx
+++ src/app/search/[searchTerm]/page.jsx
@@ -1,5 +1,7 @@
+"use client";
+
 import { useState, useEffect } from 'react';
-import Results from '../components/Results.jsx';
+import Results from '../components/Results';
 
 export default function SearchPage({ params }) {
   const searchTerm = params.searchTerm;
